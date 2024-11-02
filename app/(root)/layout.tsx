127830f2cfd66f7f307833f1db0c22f0fa82d9ee
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2024-11-02 12:51:52 -0300

    Adding sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,27 @@
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNavBar from "@/components/MobileNavBar";
 
 export default function RootLayout({children}: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const loggedInUser = {
+        firstName: "John",
+        lastName: "Doe",
+    }
+
     return (
-        <main>
-            SIDEBAR
-            {children}
+        <main className="flex h-screen w-screen font-inter">
+            <Sidebar user={loggedInUser} />
+            <div className="flex size-full flex-col">
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+                    <MobileNavBar user={loggedInUser} />
+                </div>
+                {children}
+            </div>
+
         </main>
     );
 }
